66e770dcecbf74d5b717ae92027d7c7a3de175ff
Evan Gupta <135003811+EvanGPA@users.noreply.github.com>
2024-06-21 14:05:41 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,42 @@
+import { Button } from '@/components/ui/button';
+import Image from "next/image";
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+
+export default function Home() {
+  return (
+    <main className= "bg-[#26313c] h-screen flex items-centre justify-centre p-10">
+  <div className = "grid w-full h-full grid-cols-1 bg-white md:grid-cols-2">
+    <div className = "bg-[#16202a] text-white flex items-center justify-center flex-col">
+      <div className = "my-4">
+        <h1 className= "text-3x1 font-semibold">Login</h1>
+        <p className = "mb-4 mt-2 text-xs text-slate-400">
+          {''}
+          Welcome to a new Content Management System
+        </p>
+        </div>
+        <form>
+          <Label htmlFor="email">Email</Label>
+          <Input className="mt-2 mb-4 bg-transparent rounded-full"
+          type="email" id="email" placeholder="Email" />
+
+          <Label htmlFor="email">Password</Label>
+          <Input className="mt-2 mb-4 bg-transparent rounded-full"
+          type="password" id="password" placeholder="Password" />
+           <Button type='submit' className='w-full mt-6 bg-indigo-600 rounded-full hover:bg-indigo-700'>
+            Login
+           </Button>
+        </form>
+        <div className='relative hidden md:block'>
+          <Image className='object-cover' fill={true} 
+          
+          src = '/bg.jpeg'
+          alt = 'claming background image'
+
+          />
+        </div>
+    </div>
+  </div>
+    </main>
+  );
+}
